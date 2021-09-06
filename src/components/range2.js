e153bdcd2bb0367cc15644f1f7d969006e7bfcd3
Miguel Hernández <contacto@hernandezmiguel.es>
2021-09-06 19:46:31 +0200

    movimiento bullet

--- src/components/range2.js
+++ src/components/range2.js
@@ -4,84 +4,38 @@ import { getLeft, getPercent } from "../util/util"
 export default Range = ({rangeValues, onChange}) => {
     const [values, setValues] = useState([])
     const sliderRef = useRef()
+    const [sliderRect, setSliderRect] = useState()
+    const [sliderWidth, setSliderWidth] = useState(200)
+    const [bullet1, setBullet1] = useState({min: 0, max: 0})
+    const [bullet2, setBullet2] = useState({min: 0, max: 0})
 
-    const [dragging, setDragging] = useState()
-
-    const bullet1Ref = useRef()
-    const bullet2Ref = useRef()
-    const bullet1Pos = useRef()
-    const bullet2Pos = useRef()
-
-    
-    const [positionBullet1, setPositionBullet1] = useState(0)
-    const [positionBullet2, setPositionBullet2] = useState(0)
-
-
+    const [bullet1Pos, setBullet1Pos] = useState(0)
+    const [bullet2Pos, setBullet2Pos] = useState(100)
+ 
     useEffect(() => {
         setValues(rangeValues.sort((a, b) => a-b))
     }, [rangeValues])
 
     useEffect(() => {
-        if(!!dragging){
-            console.log("Add mousemove")
-            console.log("Add mouseup")
-            document.addEventListener("mousemove", mouseMove)
-            document.addEventListener('mouseup', mouseUp);
-        return  () => {
-            console.log("Remove mousemove")
-            document.removeEventListener("mousemove", mouseMove);
+        if(sliderRef.current){
+            const rect = sliderRef.current.getBoundingClientRect()
+            setSliderRect(rect)
+            setSliderWidth(sliderRef.current.offsetWidth)
         }
-        }else{
+    }, [sliderRef.current])
 
+    useEffect(() => {
+        if(sliderRect){
+            setBullet1({min: sliderRect.left, max: sliderRect.left + bullet2Pos, width:sliderWidth})
+            setBullet2({min: sliderRect.left + bullet1Pos, max: sliderRect.right, width:sliderWidth})
         }
-    }, [dragging])
-
-    const mouseMove = (event) => {
-        
-        const left = sliderRef.current.getBoundingClientRect().left
-        if(dragging === "bullet1"){
-            let newPosX = event.clientX - bullet1Pos.current - left
-            const end = sliderRef.current.offsetWidth - bullet1Ref.current.offsetWidth
-            const start = 0
-            if(newPosX < start){
-                newPosX = 0
-            }
-            if(newPosX > end){
-                newPosX = end
-            }
-            const percent = getPercent(newPosX, end)
-            bullet1Ref.current.style.left = getLeft(percent)
-            console.log("dragging", percent)
-        }
-        if(dragging === "bullet2"){
-            let newPosX = event.clientX - bullet2Pos.current - left
-        }
-        
-    }
-
-    const mouseUp = () => {
-        setDragging(undefined)
-        console.log("Remove mouseup")
-        document.removeEventListener('mouseup', mouseUp);
-    }
-
-    const mouseDown = (event, bullet) => {
-        console.log("mousedown")
-        setDragging(bullet)
-        if(bullet === "bullet1"){
-            bullet1Pos.current = event.clientX - bullet1Ref.current.getBoundingClientRect().left
-        }
-        if(bullet === "bullet2"){
-            bullet2Pos.current = event.clientX - bullet2Ref.current.getBoundingClientRect().left
-        }
-    }
-
+    }, [sliderRect])
 
     return <div className="range">
         <div className="range-wrapper">
             <Label text={values.length > 0 ? values[0] : ""} />
             <Slider ref={sliderRef}>
-                <Bullet ref={bullet1Ref} onMouseDown={(ev) => mouseDown(ev, "bullet1")} />
+                {sliderRect && <Bullet dimensions={bullet1} onChange={setBullet1Pos} />}
                 {/* <Bullet ref={bullet2Ref} onMouseDown={(ev) => mouseDown(ev, "bullet2")} /> */}
                 {
                     values.map(v => <Track key={v} />)
@@ -105,8 +59,63 @@ const Track = React.forwardRef(({className, ...rest}, ref) => {
     return <span {...rest} className={`track ${className || ""}`.replace(" ", "")} />
 })
 
-const Bullet = React.forwardRef(({position, className, ...rest}, ref) => {
-    return <span {...rest} className={`bullet ${className || ""}`.replace(" ", "")} ref={ref} />
+const Bullet = React.forwardRef(({dimensions,values, onChange, className, ...rest}, ref) => {
+    const [dragging, setDragging] = useState(false)
+    const bulletRef = useRef()
+    const bulletPos = useRef()
+
+    const {min, max, width} = dimensions
+
+    console.log(min, max, width)
+
+    useEffect(() => {
+        if(dragging){
+            console.log("Add mousemove")
+            console.log("Add mouseup")
+            document.addEventListener("mousemove", mouseMove)
+            document.addEventListener('mouseup', mouseUp);
+            return  () => {
+                console.log("Remove mousemove")
+                document.removeEventListener("mousemove", mouseMove);
+            }
+        }
+    }, [dragging])
+
+    const mouseMove = (event) => { 
+        if(dragging){
+            let newPosX = event.clientX - bulletPos.current - min
+            const end = (max - min) - bulletRef.current.offsetWidth
+            const start = 0
+            if(newPosX < start){
+                newPosX = start
+            }
+            if(newPosX > end){
+                newPosX = end
+            }
+            const maxPercent = end * 100 / width
+            const percent = newPosX * maxPercent / end // getPercent(newPosX, end)
+            const newValue = getLeft(percent)
+            bulletRef.current.style.left = newValue
+            if(onChange){
+                onChange(newPosX)
+            }
+        }
+        
+    }
+
+    const mouseUp = () => {
+        setDragging(false)
+        console.log("Remove mouseup")
+        document.removeEventListener('mouseup', mouseUp);
+    }
+
+    const mouseDown = (event) => {
+        setDragging(true)
+        bulletPos.current = event.clientX - bulletRef.current.getBoundingClientRect().left
+    }
+
+
+    return <span {...rest}  onMouseDown={mouseDown} className={`bullet ${className || ""}`.replace(" ", "")} ref={bulletRef} />
 })
 
 const Label = React.forwardRef(({className, text, ...rest}, ref) => {
