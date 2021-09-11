2c42b545be8d88190eff5b150d815fd69cf3ebc0
Miguel Hernández <contacto@hernandezmiguel.es>
2021-09-11 17:07:44 +0200

    add clickOnLabel parameter

--- src/app/home.js
+++ src/app/home.js
@@ -8,12 +8,14 @@ export const Home = () => {
         push(path)
     }
 
+    const cambio = (values) => {
+        console.log("Recibido el cambio: ", values)
+    }
     return <>
         <button onClick={() => change("/exercise1")} >Go to Exercise 1</button>
         <button onClick={() => change("/exercise2")} >Go to Exercise 2</button>
         <div> 
-            <Range min={0} max={100} />
-
+            <Range clickOnLabel rangeValues={[1.99, 5.99, 10.99, 30.99, 50.99, 70.99]} onChange={cambio} />
         </div>
     </>
 }
--- src/components/range.js
+++ src/components/range.js
@@ -1,48 +1,159 @@
-import React, {useEffect, useState, useRef, forwardRef, useImperativeHandle} from "react"
+import React, {useEffect, useState, useRef, forwardRef, useCallback, useImperativeHandle} from "react"
 import PropTypes from 'prop-types';
 import { getOfsetOfValue, getNextValue, getOffsetValues, getRealValue } from "../util/util"
 
+/**
+ * Custom hook para transformar entre unidades y porcentajes
+ * @param width: Ancho del slider
+ * @param min: valor mínimo
+ * @param max: valor máximo
+ */
+const useConverter = (width, min, max) => {
+    const percentToValue = (value = 0) => {
+        if(typeof width !== undefined){
+            const pixels = value * width / 100
+            const resultado =  (pixels * (max - min) / width) + min
+            return resultado
+        }
+    }
 
-const Range = ({onChange, defaultValue, rangeValues, min, max, ...rest}) => {
+    const valueToPercent = (value) => {
+        if(typeof width !== undefined){
+            const real = (value - min) * width / (max - min)
+            const resultado = real * 100 / width
+            return resultado
+        }
+    }
+
+    return { percentToValue, valueToPercent }
+}
 
 
+const Range = ({onChange, defaultValue, rangeValues, min = 0, max= 100, clickOnLabel, ...rest}) => {
+    const sliderRef = useRef()
+    const valuesRef = useRef()
+    const [sliderRect, setSliderRect] = useState()
+    const [minimo, setMinimo] = useState(0)
+    const [maximo, setMaximo] = useState(100)
 
+    const [bullet1, setBullet1] = useState()
+    const [bullet2, setBullet2] = useState()
+    const bullet1Ref = useRef()
+    const bullet2Ref = useRef()
 
+    const { percentToValue, valueToPercent } = useConverter(sliderRect?.width || 200, minimo, maximo)
 
+    useEffect(() => {
+        const rect = sliderRef.current?.getBoundingClientRect()
+        setSliderRect(rect)
+        reset()
+    }, [sliderRef?.current])
 
+    useEffect(() => {
+        if(bullet1Ref?.current && bullet2Ref.current){
+            bullet1Ref.current.update(bullet1.value)
+            bullet2Ref.current.update(bullet2.value)
+        }
+        if(typeof bullet1?.value === "undefined" || typeof bullet2?.value === "undefined"|| typeof onChange === "undefined") return 
+        onChange({
+            min: percentToValue(bullet1.value),
+            max: percentToValue(bullet2.value)
+        })
+    }, [bullet1, bullet2])
+
+    const reset = () => {
+        const minimoAbsoluto = Array.isArray(rangeValues) && rangeValues.length > 0 ? 
+        rangeValues[0] : min || 0;
+        const maximoAbsoluto = Array.isArray(rangeValues) && rangeValues.length > 0 ? 
+            rangeValues[rangeValues.length - 1] : max || 100;
+        const valorBullet1 = typeof defaultValue?.min !== "undefined" && defaultValue.min >=  minimoAbsoluto ? 
+            defaultValue.min : 
+            minimoAbsoluto;
+        const valorBullet2 = typeof defaultValue?.max !== "undefined" && defaultValue.max <=  maximoAbsoluto ? 
+            defaultValue.max : 
+            maximoAbsoluto;
+        setMinimo(minimoAbsoluto)
+        setMaximo(maximoAbsoluto)
+        setBullet1({
+            min: valueToPercent(minimoAbsoluto),
+            max: valueToPercent(maximoAbsoluto),
+            value: valueToPercent(valorBullet1)
+        })
+        setBullet2({
+            min: valueToPercent(minimoAbsoluto),
+            max: valueToPercent(maximoAbsoluto),
+            value: valueToPercent(valorBullet2)
+        })
+        valuesRef.current = {min: valorBullet1, max: valorBullet2}
+    }
+
+    const normalizar = (value) => {
+        const real = percentToValue(value)
+        if(Array.isArray(rangeValues) && rangeValues.length > 0){
+            const proximo = rangeValues
+                .reduce((prev, curr) => Math.abs(curr - real) < Math.abs(prev - real) ? curr : prev )
+            return valueToPercent(proximo)
+        }
+        const proximo = Math.round(real * 100) / 100
+        return valueToPercent(proximo)
+    }
+
+    const change = (value, bullet) => {
+        const normalizado = normalizar(value)
+        if(bullet === "bullet1"){
+            setBullet1(prev => ({...prev, value: normalizado}))
+            setBullet2(prev => ({...prev, min: normalizado}))
+        }else if(bullet === "bullet2"){
+            setBullet2(prev => ({...prev, value: normalizado}))
+            setBullet1(prev => ({...prev, max: normalizado}))
+        }
+        
+    }
 
     
     return (
         <div {...rest} className={`${rest.className|| ""} range`.replace(" ", "")} >
             <div className="range-wrapper">
-            <Label text={min?.toString() || ""} onClick={() => handleChange(0, "bullet1")} />
-            <Slider>
-                <Bullet dimensions={{min, max}} />
-                <Bullet dimensions={{min, max}} />
+            <Label 
+                text={minimo} 
+                onClick={!clickOnLabel ? undefined : () => change(valueToPercent(minimo), "bullet1")} 
+            />
+            <Slider ref={sliderRef}>
+                {sliderRect &&
+                    <>
+                        <Bullet 
+                            values={bullet1} 
+                            sliderRect={sliderRect}
+                            onChange={(value) => change(value, "bullet1")}
+                            ref={bullet1Ref}
+                        />
+                        <Bullet 
+                            values={bullet2} 
+                            sliderRect={sliderRect}
+                            onChange={(value) => change(value, "bullet2")}
+                            ref={bullet2Ref}
+                        />
+                    </>
+                }
             </Slider>
-            <Label text={min?.toString() || ""} onClick={() => handleChange(0, "bullet1")} />
+            <Label 
+                text={maximo} 
+                onClick={!clickOnLabel ? undefined : () => change(valueToPercent(maximo), "bullet2")} 
+            />
             </div>
         </div>
     )
 }
 
-const Slider = React.forwardRef(({children, className, ...rest}, ref) => {
-    return <div className="slider-wrapper" >
-        <div {...rest}  className={`slider ${className || ""}`.replace(" ", "")} ref={ref}>
-            {children}
-        </div>
-    </div>
-})
-
-const Bullet  = forwardRef(({dimensions, slideRect, value, onChange, className, ...rest}, ref) => {
-    console.log(dimensions)
+const Bullet  = forwardRef(({values, sliderRect, onChange, className, ...rest}, ref) => {
     const [dragging, setDragging] = useState(false)
     
     const bulletRef = useRef()
     const offsetBulletRef = useRef()
     const lastPosX = useRef()
+    const bulletWidthRef = useRef(10)
 
-    const {min, max} = dimensions
+    const {min, max, value} = values
 
     useImperativeHandle(
         ref,
@@ -53,6 +164,13 @@ const Bullet  = forwardRef(({dimensions, slideRect, value, onChange, className, 
     )
 
     useEffect(() => {
+        if(bulletRef?.current){
+            bulletWidthRef.current = bulletRef.current.getBoundingClientRect().width
+            console.log(bulletWidthRef.current)
+        }
+    }, [bulletRef.current])
+
+    useEffect(() => {
          move(value)
     }, [value])
 
@@ -60,31 +178,9 @@ const Bullet  = forwardRef(({dimensions, slideRect, value, onChange, className, 
         if(dragging){
             document.addEventListener("mousemove", mouseMove)
             document.addEventListener('mouseup', mouseUp);
-            return  () => {
-                document.removeEventListener("mousemove", mouseMove);
-            }
+            return  () => document.removeEventListener("mousemove", mouseMove);
         }
     }, [dragging])
-
-    const mouseMove = (event) => { 
-        if(dragging){
-            const newPosX = event.clientX - slideRect.left
-            let percent = newPosX * 100 / slideRect.width
-            if(percent < min){
-                percent = min
-            }else if(percent > max){
-                percent = max
-            }
-            move(percent)
-        }
-        
-    }
-
-    const move = (value) => {
-        const pos = `calc(${value}% - ${bulletRef.current.getBoundingClientRect().width / 2}px)`
-        bulletRef.current.style.left = pos
-        lastPosX.current = value
-    }
 
     const mouseUp = () => {
         document.removeEventListener('mouseup', mouseUp);
@@ -99,6 +195,26 @@ const Bullet  = forwardRef(({dimensions, slideRect, value, onChange, className, 
         offsetBulletRef.current = event.clientX - bulletRef.current.getBoundingClientRect().left
     }
 
+    const mouseMove = (event) => { 
+        if(dragging){
+            const newPosX = event.clientX - sliderRect.left
+            let percent = newPosX * 100 / sliderRect.width
+            if(percent < min){
+                percent = min
+            }else if(percent > max){
+                percent = max
+            }
+            move(percent)
+        }
+        
+    }
+
+    const move = (value) => {
+        const pos = `calc(${value}% - ${bulletWidthRef.current / 2}px)`
+        bulletRef.current.style.left = pos
+        lastPosX.current = value
+    }
+
 
     return <span 
         {...rest}
@@ -109,9 +225,16 @@ const Bullet  = forwardRef(({dimensions, slideRect, value, onChange, className, 
     />
 })
 
+const Slider = React.forwardRef(({children, className, ...rest}, ref) => {
+    return <div className="slider-wrapper" >
+        <div {...rest}  className={`slider ${className || ""}`.replace(" ", "")} ref={ref}>
+            {children}
+        </div>
+    </div>
+})
 
 const Label = ({className, text, onClick, ...rest}) => {
-    return <span {...rest} style={{cursor: "pointer"}} onClick={onClick} className={`label ${className || ""}`.replace(" ", "")}>
+    return <span {...rest} style={{cursor: onClick ? "pointer" : "default"}} onClick={onClick} className={`label ${className || ""}`.replace(" ", "")}>
         {text}
     </span>
 }
@@ -119,9 +242,10 @@ const Label = ({className, text, onClick, ...rest}) => {
 Range.prototype = {
     onChange: PropTypes.func,
     defaultValue: PropTypes.shape({min:PropTypes.number, max:PropTypes.number}),
-    rangeValues: PropTypes.arrayOf(PropTypes.number) ,
+    rangeValues: PropTypes.arrayOf(PropTypes.number),
     min: PropTypes.number.isRequired,
     max: PropTypes.number.isRequired,
+    clickOnLabel: PropTypes.bool
 }
 
 export default Range;
